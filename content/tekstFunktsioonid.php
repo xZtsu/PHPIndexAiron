9a4c1172b7b4dc8ff5554347a3ead0cb092eb286
xZtsu <116342432+xZtsu@users.noreply.github.com>
2025-12-01 14:27:08 +0200

    tekstifunktsioonid

--- content/tekstFunktsioonid.php
+++ content/tekstFunktsioonid.php
@@ -1,53 +1,89 @@
 <?php
-function clearVarsExcept($url, $varname) {
-    // basename - makes the link relative, url must contain a filename that it returns basename('http://www.ee/index.php') > index.php
-    $url = basename($url);
-    if (str_starts_with($url, "?")) {
+function clearVarsExcept($url, $varname){
+    $url=basename($url);
+    if(str_starts_with($url,"?")){
         return "?$varname=".$_REQUEST[$varname];
     }
-    // strtok - returns first token after spliting on separator "?" strtoken('index.php?haha=lala', '?') > index.php
     return strtok($url, "?")."?$varname=".$_REQUEST[$varname];
 }
-$tekst="PHP on skriptikeel serveri pool";
-echo "<h2>Tekst funktsioonid</h2>";
+echo "<h2>Tekstifunktsioonid</h2>";
+$tekst='Veebirakendused on arvutitarkvara programm';
+echo $tekst; //näitab muutaja sisu
+echo "<br>";
+echo "Mitu sõna on lauses -str_word_count()= " .str_word_count($tekst). "tk";
+//metshein.com -->PHP alused
+echo "<br>";
+echo "Kõik tähed muudab väiksemaks -strolower ".strtolower($tekst);
+echo '<br>';
+echo "Kõik tähed muudab väiksemaks -strtoupper ".strtoupper($tekst);
+echo '<br>';
+$tekst = 'Life Is About Ignoring The Drama.';
+
+echo ucfirst(strtolower($tekst));
+echo '<br>';
+//Tekstipikkus
+$tekst = 'Experience is the teacher of fools';
+echo strlen($tekst);          //34
+echo '<br>';
+//Tekst kaks
+$tekst2 = '     A woman should soften but not weaken a man   ';
+echo "<pre>$tekst2 </pre>";
+echo "Eemaldab ühikud tekstist -trim()<pre>".trim($tekst2)."</pre>";
+echo "Eemaldab ühikud teksti eest -ltrim()<pre>".ltrim($tekst2)."</pre>";
+echo "Eeemaldab teksti pärast -rtrim()<pre>".rtrim($tekst2)."</pre>";
+echo "<h3>Tekst kui massiv</h3>";
+
+echo "Tekst: ";
 echo $tekst;
-echo "<br>";
-echo "Teksti pikkus (strlen()) on: ".strlen($tekst). " tähte";
-echo "<br>";
-echo "Esimesed 6 tähte on (substr()): ".substr($tekst,0,6);
-echo "<br>";
-echo "Alates 6 tähest on (substr()): ".substr($tekst,6);
-echo "<br>";
-echo "Sõnade arv lauses on (str_word_count()): ".str_word_count($tekst). " tk";
-echo "<br>";
-echo "Esimese tühiku asukoht on peale (strpos(): ". strpos($tekst, " "). " sümboolit" ;
-echo "<br>";
-echo " Näita kõik sümboolid peale esimese tühiku: ";
-echo substr($tekst, strpos($tekst, " "));
-echo "<br>";
-echo "Kõik tähed on väiksed (strtolower) - ".strtolower($tekst);
-echo "<br>";
-echo "Kõik tähed on suured (strtoupper) - ".strtoupper($tekst);
-echo "<br>";
-echo "Iga sõna lauses algab suure tähega (ucwords) - ".ucwords($tekst);
-
-echo "<br>";
-echo "Mõistatus. Õppeaine.";
-// PHP-Tekstfunktsioonid -https://www.metshein.com/unit/php-tekstifunktsioonid-ulesanne-9/
-
-//Дай 6 подсказок с использованием  текстовых функций,
-// чтобы отгадать название предмета.
-//добавить проверку через ввод в текстовое поле
-echo "<br>";
-$aine="";
-echo "<ol>";
-echo "<li>Количество символов в названии предмета ".strlen($aine). "</li>";
-
+echo "</br>";
+echo "võtab tekstist esimese tähe -tekst[0]= ".$tekst[0];            //A
+echo '<br>';
+echo "võtab tekstist viienda tähe -tekst[4]= ".$tekst[4];            //t
+echo '<br>';
+echo "Võtab alates neljast sümbolist 5 tähte -substr(tekst, 3, 5)=".substr($tekst, 3, 5);      //thin
+echo '<br>';
+echo " Võtab alates viiendast tähest kuni 13 täheni -substr(tekst, 4, -13)".substr($tekst, 4, -13); //thinking men
+echo '<br>';
+echo "Võtab paremalt poolt alates 8,7 tk -substr(tekst, -8, 7); ".substr($tekst, -8, 7);       //atheist
+echo '<br>';
+print_r(str_word_count($tekst, 1));
+echo '<br>';
+$sona = str_word_count($tekst, 1);
+echo $sona[2];
+echo"Võtab kolmanda sõna tekstimassiivist -sona[2]=";
+echo '<br>';
+echo "<h3>Teksti asendamine</h3>";
+$asendus = 'Tarkvara asendamine';
+$otsitav_algus = 4;
+$otsitav_pikkus = 4;
+echo substr_replace($tekst, $asendus, $otsitav_algus, $otsitav_pikkus);
+echo '<br>';
+$otsi = array('on', 'programm');
+$asenda = array('---', 'software', 'ees');
+echo "otsib sõnu= on ja asendab sõna= programm - str_replace(otsi, asendab, tekst)=".str_replace($otsi, $asenda, $tekst);
+//Mõistatus - arva ära EESTI LINNANIMI
+// eesmärk on ära arvata, millist Eesti linna on kirjeldatud.
+// Kirjuta abiks 5-6 tekstipõhist "funktsiooni" ehk vihjet,
+// mis aitaavad samm-sammult lähemale jõuda õigele linnaimele.
+// funktsioonid on näitkes selliseid - esimene täht on ..jne*/
+echo '<br>';
+$linn="Tallinn";
+echo "<ol><li>Linn algab ".substr($linn, 0,1)." tähega</li>";
+echo "<li>Linna nimes on ".strlen($linn)." tähte</li>";
+echo "<li>Viimane täht on: ".substr($linn, -1)."</li>";
+echo "<li>Keskmine osa nimest on: ".substr($linn, 2, 2)."</li>";
 echo "</ol>";
 ?>
-<form name="tekstkontroll" action="<?=clearVarsExcept($_SERVER['REQUEST_URI'], "leht")?>" method='post'>
-    <label for="tekst">Arv1</label>
-    <input type="number" name="tekst" id="tekst" min="0" max="10" step="1">
-    <br>
-    <input type="submit" value="Kontrolli">
-</form>
+    <form action="<?=clearVarsExcept($_SERVER['REQUEST_URI'],"leht")?>" method="post">
+        <label for="linn">Sisesta linnanimi</label>
+        <input type="text" id="linn" name="linn">
+        <input type="submit" value="Kontrolli">
+    </form>
+<?php
+if(isset($_REQUEST["linn"])){
+    if($_REQUEST["linn"]=="Tallinn"){
+        echo  $_REQUEST["linn"]. " on õige";
+    } else {
+        echo $_REQUEST["linn"]." on vale";
+    }
+}
