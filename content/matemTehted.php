ed6c8dbd6d50ff90706964e3182ec81b81328140
xZtsu <116342432+xZtsu@users.noreply.github.com>
2025-12-01 17:20:59 +0200

    Update matemTehted.php

--- content/matemTehted.php
+++ content/matemTehted.php
@@ -1,50 +1,93 @@
 <?php
-// eemalda urlist muutujad
-
-function clearVarsExcept($url, $varname) {
-    // basename - makes the link relative, url must contain a filename that it returns basename('http://www.ee/index.php') > index.php
-    $url = basename($url);
-    if (str_starts_with($url, "?")) {
+function clearVarsExcept($url, $varname){
+    $url=basename($url);
+    if(str_starts_with($url, "?")){
         return "?$varname=".$_REQUEST[$varname];
     }
-    // strtok - returns first token after spliting on separator "?" strtoken('index.php?haha=lala', '?') > index.php
     return strtok($url, "?")."?$varname=".$_REQUEST[$varname];
+
 }
-    echo "<h2>Matemaatilised tehted</h2>";
-    echo "<a href='https://www.metshein.com/unit/php-matemaatilised-tehted-ulesanne-2/'>
-    PHP matemaatilised tehted</a>";
-
-//mõistatus
-echo "<h2>Загадали 2 числа до ...</h2>";
-$arv1=5;
-$arv2=5;
-echo "<ul>";
-echo "<li>Если из первого числа вычесть второе, то будет  ".($arv1-$arv2)."</li>";
-echo "<li>Если первое возвести в степень 3".pow($arv1, 3)."</li>";
-//в подсказках перебрать все матем операции
-//пару операторов присваивания (omistamise operaatorid)
-
-
-echo "</ul>";
-echo $arv1. " ja ". $arv2. "<br>";
-echo "<h2>Vastuste kontroll</h2>";
+echo "<h2>Matemaatilised tehted/funktsioonid</h2>";
+$arv1 = 100;
+$arv2 = 15;
+$liitmine=$arv1+$arv2;
+$lahutamine=$arv1-$arv2;
+$korrutis=$arv1*$arv2;
+$jagamine=$arv1/$arv2;
+echo "arv1 on ".$arv1." ja arv2 on ".$arv2."<br>";
+echo "<strong>Vastused:</strong> Liitmine ".$liitmine."<br>";
+echo "Lahutaamine: ".$lahutamine."<br>";
+echo "Korrutis: ".$korrutis."<br>";
+echo "Jagamine: ".$jagamine."<br>";
+echo "Omistamise operaatorid: ";
+echo "<br>";
+// $arv1++ - suurendamine ühe võrra $arv1=$arv1+1
+$arv1++;
+echo $arv1." - suurendamine ühe võrra";
+echo "<br>";
+// $arv1-- - vähendamine ühe võrra $arv1=$arv1-1
+$arv1--;
+echo $arv1." - vähendamine ühe võrra";
+echo "<br>";
+echo "<strong>Ruutjuur -sqrt()</strong>  = ".sqrt($arv1);
+echo "<br>";
+$arvud = array(11,21,32,43,54);
+echo "Suurim arv massiivist - ".max($arvud);
+echo "<br>";
+echo "Väikseim arv massiivist - ".min($arvud);
+echo "<br>";
+echo "Juhuslik arv - ".rand();
+echo "<br>";
+$arv3 = 3.456;
+echo "Ümardamine - ".round($arv3);
+echo "<br>";
+$a = 12;
+$b = 10;
+$pindala = 2*($a + $b);
+echo "Ristküliku pindala arvudega 12 ja 10 = ".$pindala;
+echo "<br>";
+echo "Arvu 5 ruut = ".pow(5,2);
+echo "<br>";
+echo "<h2>Arvmõistatus. Arva ära kaks arvu vahemikus 0-10</h2>";
+$slaarv1 = 9;
+$slaarv2 = 5;
+//kiruta matemaatiliste tehtede või funktsioonide abil 5 vihje
+echo "<ol><li>Kui esimene arv korrutada 5, siis tuleb ".($slaarv1*5)."</li>";
+echo "<li>Kui teine arv korrutada 5, siis tuleb ".($slaarv2*5)."</li>";
+echo "<li>Kui arvud liita kokku, siis tuleb  ".($slaarv1+$slaarv2)."</li>";
+echo "<li>Kui arvud lahutada, siis tuleb  ".($slaarv2-$slaarv1)."</li>";
+echo "<li>Kui arvud jagada, siis tuleb  ".($slaarv2/$slaarv1)."</li></ol>";
 ?>
-    <form name="arvud" action="<?=clearVarsExcept($_SERVER['REQUEST_URI'], "leht")?>" method='post'>
-        <label for="arv1">Arv1</label>
-        <input type="number" name="arv1" id="arv1" min="0" max="10" step="1">
+    <form action="<?=clearVarsExcept($_SERVER['REQUEST_URI'], "leht")?>" method="post">
+        <label for="arv1">Arv1: </label>
+        <input type="number" id="arv1" name="arv1" min="0" max="10" step="1">
         <br>
-        <label for="arv2">Arv2</label>
-        <input type="number" name="arv2" id="arv2" min="0" max="10" step="1">
+        <label for="arv2">Arv2: </label>
+        <input type="number" id="arv2" name="arv2" min="0" max="10" step="1">
         <input type="submit" value="Kontrolli">
-    </form>
+    </form
 <?php
-if(isset($_REQUEST["arv1"])){
-    if($_REQUEST["arv1"]==$arv1){
-        if($_REQUEST["arv2"]==$arv2){
+if(isset($_REQUEST["arv1"]) && $_REQUEST["arv1"]!=""){
+    if($_REQUEST["arv1"] == $slaarv1){
+        echo "<span id='correct'>";
+        echo $_REQUEST["arv1"]." on õige!";
+        echo "</span>";
+    } else {
+        echo "<span id='incorrect'>";
+        echo $_REQUEST["arv1"]." on vale!";
+        echo "</span>";
+    }
 
-            echo "õige";
-            echo "<body style='background-color: lightgreen'>";
-        }
+}
+echo "<br>";
+if(isset($_REQUEST["arv2"]) && $_REQUEST["arv2"]!=""){
+    if($_REQUEST["arv2"] == $slaarv2){
+        echo "<span id='correct'>";
+        echo $_REQUEST["arv2"]." on õige!";
+        echo "</span>";
+    } else {
+        echo "<span id='incorrect'>";
+        echo $_REQUEST["arv2"]." on vale!";
+        echo "</span>";
     }
 }
-highlight_file('matemTehted.php');
